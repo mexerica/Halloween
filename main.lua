9cb2a9321c3bf1aa0cf7c14367ddbcf1b84924e3
tiago <82677798+mexerica@users.noreply.github.com>
2023-10-17 22:07:58 -0300

    só o planejamento por enquanto

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
 function love.draw()
-    love.graphics.print("Só deixando tudo pronto pra amanha pq ta tarde |^-^|", 400, 300)
+    love.graphics.print("Só deixando tudo pronto pra amanha pq ta tarde |^-^|", 0, 0);
 end
+ 
